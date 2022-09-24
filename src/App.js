377b24ecca8be43ea621103764618116b3e77147
AlAmin1097 <amin15-1097@diu.edu.bd>
2022-09-24 11:11:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import JobLists from "./components/Jobs/JobLists";
+import JobDetails from "./components/Job-details/JobDetails";
+
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/jobs" />} />
+        <Route path="/jobs" element={<JobLists />} />
+        <Route path="/jobs/:position" element={<JobDetails />} />
+      </Routes>
     </div>
   );
 }
